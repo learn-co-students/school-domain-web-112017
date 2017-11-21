30cb420ad45f19eb1688fd35bb75e23e86e301e9
ddbrennan <ddbren@gmail.com>
2017-11-21 15:05:49 -0500

    add school class methods and roster

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !@roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort
+    end
+  end
+
+end
