65b6e6f0b93432f986116a97623d1742315bf29d
iamm28 <alisonmackay246@gmail.com>
2017-11-22 11:36:23 -0500

    made school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student,grade)
+    #@roster[grade] = []
+    if @roster[grade] == nil
+      @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    @roster.each do |grade,students|
+      @roster[grade] = students.sort
+    end
+    @roster.sort.to_h
+  end
+end
