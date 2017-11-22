3d156747aaca5b893b799e24f90115aa21891978
walkingalchemy <walkingalchemy@gmail.com>
2017-11-22 09:29:50 -0500

    Add School class with initialize and add student methods -- incomplete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
 # code here!
+class School
+  attr_accessor :student, :name
+  def initialize(name)
+    @roster = []
+    @name = name
+  end
+  def add_student(student, int)
+    @student = student
+    @roster << student
+  end
+end
