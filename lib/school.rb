442fe10e6e807fae89a62eabf3a92a99e0755900
walkingalchemy <walkingalchemy@gmail.com>
2017-11-22 22:00:07 -0500

    Add School class to mock up a basic ORM for the school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,30 @@
 # code here!
 class School
-  attr_accessor :student, :name
+  attr_accessor :student, :name, :roster
   def initialize(name)
-    @roster = []
+    @roster = {}
     @name = name
   end
-  def add_student(student, int)
+  def add_student(student, grade)
     @student = student
-    @roster << student
+    if @roster.include? grade
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_with_object({}) do |(grade, students), hash|
+      hash[grade] = students.sort
+    end
+    # @roster.each do |grade, students|
+    #   @roster[grade] = students.sort
+    # end
+    # @roster
   end
 end
