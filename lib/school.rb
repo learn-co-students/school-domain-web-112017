af4de621aeb9223216469fed2e81bdb39a155468
github username <siobhan.p.mahoney@gmail.com>
2017-11-23 10:51:43 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student (student, grade)
+    if !@roster.keys.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade (grade_level)
+    @roster[grade_level]
+  end
+
+  def sort
+    sorted_list = {}
+    @roster.each do |grade_hash, student_array|
+      sorted_list[grade_hash] = student_array.sort
+    end
+    sorted_list
+  end
+end
