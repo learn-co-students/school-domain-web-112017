adf7f58f5fe9f5cf62b092605aef2370f55c6461
jerrybennett <jerrysbusiness@gmail.com>
2018-04-23 13:00:00 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,41 +1,64 @@
 require "pry"
 
 class School
+  attr_accessor :roster, :name
 
-  attr_accessor :roster
-  # attr_reader :name, :grade
-
-  def initialize(roster)
+  def initialize(name)
+    @name = name
     @roster = {}
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def grade=(grade)
-    @grade = grade
-  end
-
   def add_student(name, grade)
-    if @roster.keys.include?(grade)
-      # binding.pry
-      @roster[grade] << name
-    else
-      @roster[grade] = []
-      @roster[grade] << name
-    end
-    @roster
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
-  def grade(num)
-    @roster[num]
+  def grade(grade)
+    roster[grade]
   end
 
   def sort
-    @roster.each do |k, v|
-      v.sort!
+    # binding.pry
+    sorted = {}
+    roster.each do |grade, name|
+      sorted[grade] = name.sort
     end
+    sorted
   end
-
 end
+
+
+# attr_accessor :roster
+# # attr_reader :name, :grade
+#
+# def initialize(roster)
+#   @roster = {}
+# end
+#
+# def name=(name)
+#   @name = name
+# end
+#
+# def grade=(grade)
+#   @grade = grade
+# end
+#
+# def add_student(name, grade)
+#   if @roster.keys.include?(grade)
+#     @roster[grade] << name
+#   else
+#     @roster[grade] = []
+#     @roster[grade] << name
+#   end
+#   @roster
+# end
+#
+# def grade(num)
+#   @roster[num]
+# end
+#
+# def sort
+#   @roster.each do |k, v|
+#     v.sort!
+#   end
+# end
